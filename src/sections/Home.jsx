f861bdbf0761a3ddacd9d4dff4033d54ae3ef9a9
S0Ale <pelizzoni_sale@protonmail.com>
2024-05-15 14:24:09 +0200

    Vite project init

---
+++ src/sections/Home.jsx
@@ -0,0 +1,17 @@
+const Home = () => {
+  return (
+    <main className="h-fluid flex column">
+        <div className="title">
+            <h1>Nome del Progetto</h1>
+        </div>
+        <div className="description">
+            <p>Breve descrizione dell'intento del progetto</p>
+        </div>
+        <div className="start">
+            <button id="start">START</button>
+        </div>
+    </main>
+  );
+};
+
+export default Home;
