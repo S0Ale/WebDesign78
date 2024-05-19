db21499f67b22a539c60d806570d69e262afcc50
S0Ale <78408616+S0Ale@users.noreply.github.com>
2024-05-19 15:31:41 +0200

    footer css fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <footer className="col-12 flex row transparent debug1">
+    <footer className="col-12 flex row transparent">
         <span>Share:</span>
-        <div className="flex row social-row debug2">
+        <div className="flex row social-row">
             <a href="">Facebook</a>
             <a href="">X</a>
             <a href="">Instagram</a>
