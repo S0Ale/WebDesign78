6f0754a9fc4e189e876ed16d986b7d7d5a8040d6
SimoLazza01 <lazzaro795@gmail.com>
2024-05-21 18:32:01 +0200

    Images and Article
    Added images and completed article.jsx and article.css

--- src/sections/Article.jsx
+++ src/sections/Article.jsx
@@ -2,10 +2,25 @@ import '../css/article.css';
 
 const Article = () => {
     return (
-        <div>
-        <h1>Article</h1>
-        <p>Article content</p>
-        </div>
+        <article className="h-fluid flex">
+            <div className='slide_counter flex'>
+                <p id='current_slide'>1</p>
+                <p>/</p>
+                <p id='total_slide'>5</p>
+            </div>
+            <div>
+                <div className="article_textbox flex column">
+                    <h1>Testo dell'articolo fake 1</h1>
+                    <p>Sottotitolo Sottotitolo</p>
+                    <button className='read_more circle'>READ MORE</button>
+                </div>
+            </div>
+            <div className='flex column'>
+                <button id='arrow_menu' className='circle'><span id="arrow" className="material-symbols-outlined circle">keyboard_arrow_up</span></button>
+                <button id='arrow_menu' className='circle'><span id="arrow" className="material-symbols-outlined circle">keyboard_arrow_down</span></button>
+            </div>
+        </article>
+        
     );
 };
 
