46477895fbdf52d453c155ede06b085b490bee92
S0Ale <78408616+S0Ale@users.noreply.github.com>
2024-05-21 18:51:34 +0200

    Improved basic routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Link } from "react-router-dom";
 import {
   createBrowserRouter,
+  createRoutesFromElements,
   RouterProvider,
+  Route
 } from "react-router-dom";
 
 import App from './App';
 import Home from './sections/Home';
 import Article from './sections/Article';
 
-const router = createBrowserRouter([
-  {
-    path: "/WebDesign78/",
-    element: <Home/>,
-  },
-  {
-    path: "/WebDesign78/",
-    element: <Article/>,
-  },
-
-]);
+const router = createBrowserRouter(
+  // funzione che crea un router a partire da elementi jsx (+ leggibile)
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<Home />} />
+      <Route path="article" element={<Article />} />
+    </Route>
+  ),
+  // il nostro url e' il nome della repo (github pages),  in questo modo
+  // evitiamo di nominarrlo sempre nei link e nei path
+  {basename: '/WebDesign78'}
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
