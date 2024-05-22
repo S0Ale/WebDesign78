6f356be1703858097cefbae48a88cd0ca3011a7a
Donzy2468 <mattia.donzy@gmail.com>
2024-05-22 14:52:40 +0200

    decrement + read more

--- src/sections/Article.jsx
+++ src/sections/Article.jsx
@@ -1,40 +1,51 @@
 import { useState } from 'react';
+import data from '../data/article-text.json';
 import '../css/article.css';
 
 const Article = () => {
-
-    const [id, setCount] = useState(1);
-    const maxCount = 5;  // Valore massimo per il contatore
-
-    // Funzione per incrementare il contatore
+    const minCount = 0;
+    const maxCount = 4;
+    const [id, setCount] = useState(minCount);
+    const [content, setContent] = useState('');
+    
     const increment = () => {
         setCount(prevCount => {
-        if (prevCount >= maxCount) {
-            return 1;  // Resetta il contatore a 0 se supera il valore massimo
-        } else {
-            return prevCount + 1;
-        }
+        setContent('');
+        if(prevCount >= maxCount) return 0;
+        else return prevCount+1;
         });
     };
+
+    const decrement = () => {
+        setCount(prevCount => {
+        setContent('');
+        if (prevCount <= minCount) return 4;
+        else return prevCount-1;
+        });
+    };
+
+    const addText = () => {
+        setContent(data.articles[id].content);
+      };
 
     return (
         <article className="h-fluid flex">
             <div className='slide_counter flex'>
-                <p id='current_slide'>{id}</p>
+                <p id='current_slide'>{id+1}</p>
                 <p>/</p>
-                <p id='total_slide'>5</p>
+                <p id='total_slide'>{maxCount+1}</p>
             </div>
             <div>
                 <div className="article_textbox flex column">
-                    <h1 id="title">Testo dell'articolo fake 1</h1>
-                    <h2 id="subtitle">Sottotitolo Sottotitolo</h2>
-                    <p id="content"></p>
-                    <button className='read_more circle'>READ MORE</button>
+                    <h1 id="title">{data.articles[id].title}</h1>
+                    <h2 id="subtitle">{data.articles[id].subtitle}</h2>
+                    <p id="content">{content}</p>
+                    <button className='read_more circle' onClick={addText}>READ MORE</button>
                 </div>
             </div>
             <div className='flex column'>
                 <button id='arrow_menu' className='circle' onClick={increment}><span id="arrow" className="material-symbols-outlined circle">keyboard_arrow_up</span></button>
-                <button id='arrow_menu' className='circle'><span id="arrow" className="material-symbols-outlined circle">keyboard_arrow_down</span></button>
+                <button id='arrow_menu' className='circle' onClick={decrement}><span id="arrow" className="material-symbols-outlined circle">keyboard_arrow_down</span></button>
             </div>
         </article>
         
