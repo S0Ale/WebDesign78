3c8e51bf60dc3618a965f7abea9c6b4d61d2b909
S0Ale <78408616+S0Ale@users.noreply.github.com>
2024-05-26 12:41:32 +0200

    Grid layout

--- src/sections/ArticleGrid.jsx
+++ src/sections/ArticleGrid.jsx
@@ -10,7 +10,30 @@ const ArticleGrid = () => {
         <AnimatedMain className="main-grid">
             <div className='h-fluid constraint-area' ref={constraints}>
                 <DraggableDiv className="grid article-grid" constraints={constraints}>
-                    <p>Content</p>
+                    <div className='grid-el debug1'>
+                        <div className='article debug2' id='art1'>Card</div>
+                    </div>
+                    <div className='grid-el debug1'>
+                        <div className='article debug2' id='art2'>Card</div>
+                    </div>
+                    <div className='grid-el debug1'>
+                        <div className='article debug2' id='art3'>Card</div>
+                    </div>
+                    <div className='grid-el debug1'></div>
+                    <div className='grid-el debug1'></div>
+                    <div className='grid-el debug1'></div>
+                    <div className='grid-el debug1'></div>
+                    <div className='grid-el debug1'></div>
+                    <div className='grid-el debug1'></div>
+                    <div className='grid-el debug1'></div>
+                    <div className='grid-el debug1'></div>
+                    <div className='grid-el debug1'></div>
+                    <div className='grid-el debug1'></div>
+                    <div className='grid-el debug1'></div>
+                    <div className='grid-el debug1'></div>
+                    <div className='grid-el debug1'></div>
+                    <div className='grid-el debug1'></div>
+                    <div className='grid-el debug1'></div>
                 </DraggableDiv>
             </div>
         </AnimatedMain>
