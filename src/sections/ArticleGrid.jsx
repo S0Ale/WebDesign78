602d38b777c528b355024fb9184d40b45093a9ff
S0Ale <pelizzoni_sale@protonmail.com>
2024-05-28 13:24:23 +0200

    better font management (articles)

--- src/sections/ArticleGrid.jsx
+++ src/sections/ArticleGrid.jsx
@@ -13,15 +13,26 @@ const ArticleGrid = () => {
                 <DraggableDiv className="grid article-grid" constraints={constraints}>
                     <div className='grid-el debug1'>
                         <ArticleCard id={0}>
-                            Quella<br/><span className="highlight2">IRRESISTIBILE</span><br/>
-                            <div className='flex debug2' style={{flexDirection: 'row-reverse', justifyContent: 'left'}} >voglia di <span className="highlight1">MURI</span></div>
+                            <div className="line down">Quella</div>
+                            <div className="line"><span className="highlight2">IRRESISTIBILE</span></div>
+                            <div className='line row-reverse up left'>voglia di <span className="highlight1">MURI</span></div>
                         </ArticleCard>
                     </div>
                     <div className='grid-el debug1'>
-                        <ArticleCard id={1}>Tanto i<br/><span className="highlight1">MURI</span> sono solo in <br/><span className="highlight1">AMERICA</span></ArticleCard>
+                        <ArticleCard id={1}>
+                            <div className="line down">Tanto i</div>
+                            <div className="line down"><span className="highlight1">MURI</span> sono solo in</div>
+                            <div className="line up"><span className="highlight1">AMERICA</span></div>
+                        </ArticleCard>
                     </div>
                     <div className='grid-el debug1'>
-                        <ArticleCard id={2}>Perchè i<br/><span className="highlight1">MURI</span><br/>sono solo quelli<br/><span className="highlight1">DELLE<br/>CASE</span></ArticleCard>
+                        <ArticleCard id={2}>
+                            <div className="line down">Perchè i</div>
+                            <div className="line"><span className="highlight1">MURI</span></div>
+                            <div className="line">sono solo quelli</div>
+                            <div className="line"><span className="highlight1">DELLE</span></div>
+                            <div className="line up"><span className="highlight1">CASE</span></div>
+                        </ArticleCard>
                     </div>
                     <div className='grid-el debug1'></div>
                     <div className='grid-el debug1'></div>
