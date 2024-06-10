69d3de1b848c1a152d90df90af2fd72544d1d941
S0Ale <78408616+S0Ale@users.noreply.github.com>
2024-06-10 17:02:29 +0200

    first dropdown implementation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
@@ -8,6 +9,12 @@ import GenButton from './GenButton';
 import { Logo, LogoResponsive } from './Logos';
 
 const Navbar = () => {
+    const [navOpen, setNavOpen] = useState(false); // responsive dropdown menu
+
+    const toggleNav = () => {
+        setNavOpen(!navOpen);
+    };
+
     return (
         <nav className="col-12 flex row layer1">
             <div className="col-12 flex row nav-wrapper">
@@ -15,16 +22,18 @@ const Navbar = () => {
                     <Logo />
                     <LogoResponsive />
                 </div>
-                <div className="h-fluid flex row links">
+
+                <div className={`h-fluid flex row links ${navOpen? 'nav-show' : ''}`}>
                     <Link to="/" className='nav-link'>Home</Link>
                     <Link to="/categories" className="nav-link">Categorie</Link>
                     <Link to="/about" className="nav-link">About</Link>
                 </div>
+                
                 <div className="search-bar">
                     <input type="text" placeholder="Search..." className="search-input" />
                     <FontAwesomeIcon icon={faSearch} className="search-icon" />
                 </div>
-                <GenButton className="nav-drop-btn">
+                <GenButton className="nav-toggle" onClick={toggleNav}>
                     <FontAwesomeIcon icon={faBars}/>
                 </GenButton>
             </div>
