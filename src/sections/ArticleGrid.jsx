f81e5ced874152389b74cdc5b893367e6da853cf
S0Ale <78408616+S0Ale@users.noreply.github.com>
2024-06-12 16:35:54 +0200

    no more duplicates

--- src/sections/ArticleGrid.jsx
+++ src/sections/ArticleGrid.jsx
@@ -13,10 +13,10 @@ const ArticleGrid = () => {
 
     // NON FUNZIONA SE UNO RICARICA LA PAGINA, MA SE SI NAVIGA DA UNA PAGINA ALL'ALTRA FUNZIONA
     // noto che calcola i constraints solo una volta
-    useEffect(() => {
-        console.log(window.innerWidth);
-        if(window.innerWidth > 992) startFromCenter(); // not if mobile
-    });
+    //useEffect(() => {
+        //console.log(window.innerWidth);
+        //if(window.innerWidth > 992) startFromCenter(); // not if mobile
+    //});
 
     // Voglio posizionare il div draggabile in una posizione piu' centrale:
     // uso come riferimento il grid container con l'id focus
@@ -33,98 +33,47 @@ const ArticleGrid = () => {
     return (
         <AnimatedMain className="main-grid">
             <div className='h-fluid constraint-area debug2' ref={constraints}>
-                <DraggableDiv className="grid article-grid" id="dragarea" constraints={constraints}>
-                    <div className="flex row grid-container debug4" id='focus'>
-                        <div className="grid-column">
-                            <GridModule>
-                                <ArticleCard className='medium' id={0}/>
-                                <ArticleCard className='' id={1}/>
-                                <ArticleCard className='' id={2}/>
-                                <ArticleCard className='big' id={3}/>
-                            </GridModule>
-                            <GridModule>
-                                <ArticleCard className='big' id={4}/>
-                                <ArticleCard className='' id={5}/>
-                                <ArticleCard className='' id={6}/>
-                                <ArticleCard className='medium' id={7}/>
-                            </GridModule>
-                            <GridModule>
-                                <ArticleCard className='medium' id={8}/>
-                                <ArticleCard className='' id={9}/>
-                                <ArticleCard className='' id={10}/>
-                                <ArticleCard className='big' id={11}/>
-                            </GridModule>
-                        </div>
-                        <div className="grid-column">
-                            <GridModule>
-                                <ArticleCard className='big' id={12}/>
-                                <ArticleCard className='' id={13}/>
-                                <ArticleCard className='' id={14}/>
-                                <ArticleCard className='medium' id={15}/>
-                            </GridModule>
-                            <GridModule>
-                                <ArticleCard className='medium' id={16}/>
-                                <ArticleCard className='' id={17}/>
-                                <ArticleCard className='' id={18}/>
-                                <ArticleCard className='big' id={19}/>
-                            </GridModule>
-                            <GridModule>
-                                <ArticleCard className='big' id={20}/>
-                                <ArticleCard className='' id={21}/>
-                                <ArticleCard className='' id={22}/>
-                                <ArticleCard className='medium' id={23}/>
-                            </GridModule>
-                        </div>
+                <DraggableDiv className="flex row article-grid" id="dragarea" constraints={constraints}>
+                    <div className="grid-column">
+                        <GridModule>
+                            <ArticleCard className='medium' id={0}/>
+                            <ArticleCard className='' id={1}/>
+                            <ArticleCard className='' id={2}/>
+                            <ArticleCard className='big' id={3}/>
+                        </GridModule>
+                        <GridModule>
+                            <ArticleCard className='big' id={4}/>
+                            <ArticleCard className='' id={5}/>
+                            <ArticleCard className='' id={6}/>
+                            <ArticleCard className='medium' id={7}/>
+                        </GridModule>
+                        <GridModule>
+                            <ArticleCard className='medium' id={8}/>
+                            <ArticleCard className='' id={9}/>
+                            <ArticleCard className='' id={10}/>
+                            <ArticleCard className='big' id={11}/>
+                        </GridModule>
                     </div>
-                    {
-                        // Articoli dummy (utili solo per aumentare lo spazio di nvigazione)
-                        [...Array(8)].map((_, i) => {
-                            return (
-                                <div className="flex row grid-container debug4 dummy" key={i}>
-                                    <div className="grid-column">
-                                        <GridModule>
-                                            <ArticleCard className='medium' id={0}/>
-                                            <ArticleCard className='' id={1}/>
-                                            <ArticleCard className='' id={2}/>
-                                            <ArticleCard className='big' id={3}/>
-                                        </GridModule>
-                                        <GridModule>
-                                            <ArticleCard className='big' id={4}/>
-                                            <ArticleCard className='' id={5}/>
-                                            <ArticleCard className='' id={6}/>
-                                            <ArticleCard className='medium' id={7}/>
-                                        </GridModule>
-                                        <GridModule>
-                                            <ArticleCard className='medium' id={8}/>
-                                            <ArticleCard className='' id={9}/>
-                                            <ArticleCard className='' id={10}/>
-                                            <ArticleCard className='big' id={11}/>
-                                        </GridModule>
-                                    </div>
-                                    <div className="grid-column">
-                                        <GridModule>
-                                            <ArticleCard className='big' id={12}/>
-                                            <ArticleCard className='' id={13}/>
-                                            <ArticleCard className='' id={14}/>
-                                            <ArticleCard className='medium' id={15}/>
-                                        </GridModule>
-                                        <GridModule>
-                                            <ArticleCard className='medium' id={16}/>
-                                            <ArticleCard className='' id={17}/>
-                                            <ArticleCard className='' id={18}/>
-                                            <ArticleCard className='big' id={19}/>
-                                        </GridModule>
-                                        <GridModule>
-                                            <ArticleCard className='big' id={20}/>
-                                            <ArticleCard className='' id={21}/>
-                                            <ArticleCard className='' id={22}/>
-                                            <ArticleCard className='medium' id={23}/>
-                                        </GridModule>
-                                    </div>
-                                </div>
-                            );
-                        })
-                    }
+                    <div className="grid-column">
+                        <GridModule>
+                            <ArticleCard className='big' id={12}/>
+                            <ArticleCard className='' id={13}/>
+                            <ArticleCard className='' id={14}/>
+                            <ArticleCard className='medium' id={15}/>
+                        </GridModule>
+                        <GridModule>
+                            <ArticleCard className='medium' id={16}/>
+                            <ArticleCard className='' id={17}/>
+                            <ArticleCard className='' id={18}/>
+                            <ArticleCard className='big' id={19}/>
+                        </GridModule>
+                        <GridModule>
+                            <ArticleCard className='big' id={20}/>
+                            <ArticleCard className='' id={21}/>
+                            <ArticleCard className='' id={22}/>
+                            <ArticleCard className='medium' id={23}/>
+                        </GridModule>
+                    </div>
                 </DraggableDiv>
             </div>
         </AnimatedMain>
