d1a87bc21e8a157a3eab0bb7cf87e4915293bfa8
S0Ale <78408616+S0Ale@users.noreply.github.com>
2024-06-18 17:19:17 +0200

    Added motionpath gsap plugin

--- src/scripts/gsap.js
+++ src/scripts/gsap.js
@@ -1,7 +1,9 @@
 export * from "gsap";
 export * from "gsap/ScrollTrigger";
+export * from "gsap/MotionPathPlugin";
 export * from "@gsap/react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin"
 import { useGSAP } from '@gsap/react';
-gsap.registerPlugin(ScrollTrigger, useGSAP);
+gsap.registerPlugin(ScrollTrigger, useGSAP, MotionPathPlugin);
